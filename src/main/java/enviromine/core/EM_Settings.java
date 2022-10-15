950a01cf18660cbae2dc18eba8a13c48e7c840a8
retbenwin <retbenwin@gmail.com>
2022-10-15 01:21:46 -0600

    first commit

---
+++ src/main/java/enviromine/core/EM_Settings.java
@@ -0,0 +1,7 @@
+package enviromine.core;
+
+public class EM_Settings {
+    public static final String Version = "Reborn";
+    public static final String ModID = "enviromine";
+    public static final String Name = "EnviroMine";
+}
---
+++ src/main/java/enviromine/core/EnviroMine.java
@@ -0,0 +1,27 @@
+package enviromine.core;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = EM_Settings.ModID, name = EM_Settings.Name, version = EM_Settings.Version, useMetadata = true)
+public class EnviroMine
+{
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
